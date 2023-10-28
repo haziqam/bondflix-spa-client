3360dbca3ebfa8f3ab6f8ca4337c28a4a046201c
Haziq <haziqabiyyu@gmail.com>
2023-10-28 18:50:23 +0700

    feat: initiate vite

---
+++ bondflix-spa-service/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   host: true,
+// })
+
+// https://stackoverflow.com/questions/74229258/vitereactdocker-not-working-in-container
+export default defineConfig({
+  plugins: [react()],
+  server: {
+   watch: {
+    usePolling: true,
+   },
+   host: true,
+   strictPort: true,
+   port: 5173, 
+ }
+})
