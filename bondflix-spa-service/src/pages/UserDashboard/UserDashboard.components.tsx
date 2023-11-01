d885a7b16229cb18e9ffc29e6be83a3912f28f5d
Haziq <haziqabiyyu@gmail.com>
2023-11-01 20:51:03 +0700

    feat: logout from user dashboard

--- bondflix-spa-service/src/pages/UserDashboard/UserDashboard.components.tsx
+++ bondflix-spa-service/src/pages/UserDashboard/UserDashboard.components.tsx
@@ -4,31 +4,18 @@ import { Button } from "primereact/button";
 import { logout } from "../../services/auth.service";
 import { usePageNavigation } from "../../contexts/PageNavigation";
 import { Login } from "../Login/Login";
-import { useEffect, useState } from "react";
 
 export function Something() {
     const { navigateTo } = usePageNavigation();
-    const [logoutSuccess, setLogoutSuccess] = useState(false);
-
     const handleLogout = async () => {
         // TODO: use react query to handle loading state
         const response = await logout();
         if (response.success) {
-            // No navigation here; set a flag or state
-            setLogoutSuccess(true);
+            navigateTo(<Login />);
         } else {
             console.log("Wrong!");
         }
     };
-
-    useEffect(() => {
-        // Use a flag or state to trigger navigation outside of rendering
-        // For example, you can use a "logoutSuccess" state
-        if (logoutSuccess) {
-            navigateTo(<Login />);
-        }
-    }, [logoutSuccess]);
-
     return (
         <div>
             <h1>Welcome to the dashboard</h1>
