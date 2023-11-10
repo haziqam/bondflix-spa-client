a85d1cca526c75dbc673ac00ad46f3de71d8dd2e
Haziq <haziqabiyyu@gmail.com>
2023-11-11 06:54:08 +0700

    feat: add link to dashboard in logo image

--- bondflix-spa-service/src/shared-components/Logo.tsx
+++ bondflix-spa-service/src/shared-components/Logo.tsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 export function BondflixLogo() {
-    return <img src={logo} alt="Bondflix logo" style={{ width: "150px" }} />;
+    return (
+        <Link to="/dashboard">
+            <img src={logo} alt="Bondflix logo" style={{ width: "150px" }} />
+        </Link>
+    );
 }
