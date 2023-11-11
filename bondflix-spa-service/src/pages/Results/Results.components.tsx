4b1c8b9f154fbd175848731e180d930f0c7a25f0
Haziq <haziqabiyyu@gmail.com>
2023-11-11 22:04:08 +0700

    feat: content search results style

--- bondflix-spa-service/src/pages/Results/Results.components.tsx
+++ bondflix-spa-service/src/pages/Results/Results.components.tsx
@@ -2,11 +2,12 @@ import { Card } from "primereact/card";
 import { TabMenu } from "primereact/tabmenu";
 import { Dispatch, SetStateAction, useState } from "react";
 import contentThumbnail from "../../assets/thumbnail1.jpg";
+import channelProfilePic from "../../temp-video/profile.png";
 
 export function ResultsContent() {
     const [tabActiveIndex, setTabActiveIndex] = useState(0);
     return (
-        <div>
+        <div style={{ marginBottom: "32px" }}>
             <ResultsMenuTab
                 tabActiveIndex={tabActiveIndex}
                 setTabActiveIndex={setTabActiveIndex}
@@ -74,19 +75,60 @@ function ContentResultsCard() {
                     },
                 },
             }}
-            style={{ backgroundColor: "#ececec" }}
+            style={{ backgroundColor: "#ececec", height: "250px" }}
         >
             <div style={{ display: "flex", gap: "20px" }}>
                 <img
                     src={contentThumbnail}
                     alt="Content Thumbnail"
-                    style={{ objectFit: "cover", borderRadius: "10px" }}
+                    style={{
+                        objectFit: "cover",
+                        borderRadius: "10px",
+                        width: "360px",
+                        height: "210px",
+                    }}
                 />
                 <section>
-                    <h2 style={{ marginTop: "0" }}>Content Title</h2>
-                    <p>Channel name</p>
-                    <p>Date uploaded</p>
-                    <p>Description</p>
+                    <h2 style={{ margin: "0" }}>Content Title</h2>
+                    <div>11 November, 2023</div>
+                    <div
+                        style={{
+                            display: "flex",
+                            gap: "8px",
+                            alignItems: "center",
+                            margin: "16px auto 16px auto",
+                        }}
+                    >
+                        <img
+                            src={channelProfilePic}
+                            alt="Channel profile picture"
+                            style={{
+                                width: "40px",
+                                height: "40px",
+                                borderRadius: "50%",
+                            }}
+                        />
+                        <div>Channel name</div>
+                    </div>
+                    <p
+                        style={{
+                            margin: "0",
+                            display: "-webkit-box",
+                            WebkitLineClamp: "4",
+                            WebkitBoxOrient: "vertical",
+                            overflow: "hidden",
+                        }}
+                    >
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Aspernatur deserunt tempora ipsam adipisci repellendus
+                        dolore, ipsum excepturi molestias est aperiam labore
+                        nemo quasi praesentium at illo facilis? Corporis,
+                        commodi vero? Lorem ipsum dolor sit amet consectetur,
+                        adipisicing elit. Reiciendis deserunt doloribus sapiente
+                        quasi. Iste fugit nulla nesciunt tempora ipsum magnam
+                        rem sequi? Adipisci dolorem nulla facere, maiores neque
+                        sapiente at.
+                    </p>
                 </section>
             </div>
         </Card>
