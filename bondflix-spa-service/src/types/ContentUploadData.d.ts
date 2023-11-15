7cdbf7ef6d6102970757b7ecc9f73bc21e2156b0
Haziq <haziqabiyyu@gmail.com>
2023-11-15 16:52:15 +0700

    feat: Content and category types

---
+++ bondflix-spa-service/src/types/ContentUploadData.d.ts
@@ -0,0 +1,9 @@
+type ContentUploadData = {
+    title: string;
+    description: string;
+    visibility: string;
+    genres: string[];
+    categories: string[];
+    video: File;
+    thumbnail: File;
+};
