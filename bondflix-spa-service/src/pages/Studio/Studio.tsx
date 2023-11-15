580c73a4ce426d4356dd15ce90fccb82c6ab247c
Haziq <haziqabiyyu@gmail.com>
2023-11-15 16:53:07 +0700

    feat: studio and upload page

--- bondflix-spa-service/src/pages/Studio/Studio.tsx
+++ bondflix-spa-service/src/pages/Studio/Studio.tsx
@@ -1,6 +1,42 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
+import { ContentUploadForm } from "./Studio.components";
 
 export function Studio() {
     const location = useLocation();
-    return <div>this is studio. {location.state.test}</div>;
+    const [contentUploadData, setContentUploadData] =
+        useState<ContentUploadData>({
+            title: "",
+            description: "",
+            visibility: "",
+            genres: [],
+            categories: [],
+            video: location.state.video,
+            thumbnail: location.state.thumbnail,
+        });
+
+    useEffect(() => {
+        console.log(contentUploadData.video);
+        console.log(contentUploadData.thumbnail);
+    }, []);
+    return (
+        <div>
+            <ContentUploadForm
+                contentUploadData={contentUploadData}
+                setContentUploadData={setContentUploadData}
+            />
+        </div>
+    );
+}
+
+function uploadButtonDisabled(contentUploadData: ContentUploadData) {
+    return (
+        contentUploadData.title === "" ||
+        contentUploadData.description === "" ||
+        contentUploadData.genres.length === 0 ||
+        contentUploadData.categories.length === 0 ||
+        !contentUploadData.video ||
+        !contentUploadData.thumbnail
+    );
 }
