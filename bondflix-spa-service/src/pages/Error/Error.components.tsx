ae9df963d991f71394150b47669ff35d89f81864
cettareswara <13521133@std.stei.itb.ac.id>
2023-11-15 21:33:56 +0700

    feat: update error, basic admin

--- bondflix-spa-service/src/pages/Error/Error.components.tsx
+++ bondflix-spa-service/src/pages/Error/Error.components.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useToast } from '../../hooks/useToast';
 
-export function ErrorContent() {
+interface ErrorContentProps {
+  onReturnHome: () => void; 
+}
+
+export const ErrorContent: React.FC<ErrorContentProps> = ({ onReturnHome }) => {
+  const navigate = useNavigate();
+
   return (
-    <div className="error-content">
-      <img
-        src="https://via.placeholder.com/150" 
-        alt="Error"
-        className="error-image"
-      />
-      <div className="error-text">
-        <h1 className="error-heading">Sorry, something went wrong</h1>
-        <p className="error-description">
-          We can't seem to find the page you're looking for.
-        </p>
+    <div className="error-container" style={{ backgroundImage: `url('./assets/netflix_bg.png')` }}>
+      <div className="error-content">
+        <div className="error-text">
+          <h1 className="error-heading">Sorry, something went wrong</h1>
+          <p className="error-description">
+            We can't seem to find the page you're looking for.
+          </p>
+          <button className="home-button" onClick={() => {
+            navigate("/dashboard");
+          }}>
+            Go to Homepage
+          </button>
+        </div>
       </div>
     </div>
   );
-}
-
-export default ErrorContent;
+};
