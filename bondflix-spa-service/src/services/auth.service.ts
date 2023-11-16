48147d942cc790b4f2b685579da1604f4b14b052
Haziq <haziqabiyyu@gmail.com>
2023-11-16 23:51:35 +0700

    fix: remove cookie set in client side

--- bondflix-spa-service/src/services/auth.service.ts
+++ bondflix-spa-service/src/services/auth.service.ts
@@ -1,15 +1,11 @@
 import axiosInstance from "../lib/axios/axios";
 import axios from "axios";
-import Cookies from "js-cookie";
 
 export async function login(data: LoginFormData): Promise<ResponseData> {
     try {
         const response = await axiosInstance.post("/auth/login", data, {
             withCredentials: true,
         });
-        if (response.data.success) {
-            Cookies.set("userId", response.data.data.userId);
-        }
         return response.data as ResponseData;
     } catch (error) {
         if (axios.isAxiosError(error)) {
@@ -46,9 +42,6 @@ export async function logout(): Promise<ResponseData> {
         const response = await axiosInstance.get("/auth/logout", {
             withCredentials: true,
         });
-        if (response.data.success) {
-            Cookies.remove("userId");
-        }
         return response.data as ResponseData;
     } catch (error) {
         if (axios.isAxiosError(error)) {
@@ -64,7 +57,6 @@ export async function logout(): Promise<ResponseData> {
 
 export async function register(data: RegisterFormData): Promise<ResponseData> {
     try {
-        console.log(data);
         const response = await axiosInstance.post("/auth/register", data, {
             withCredentials: true,
         });
